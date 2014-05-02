c7ee7bb42d7773cc27aa12c4e94851f80b748e34
jeffeaton <jeffrey.eaton@imperial.ac.uk>
2014-05-02 13:53:21 +0100

    very small: changed file path to library in functions.R

--- C++/functions.R
+++ C++/functions.R
@@ -8,7 +8,7 @@ DS <- 8   # number of disease stages
 TS <- 4   # ART treatment duration stages
 OUT.STEPS <- length(1970:2012)
 
-dyn.load("rlib.so")
+dyn.load("C++/rlib.so")
 fnSpectrum <- function(iota, rVec){
   return(.C("fnSpectrumR", iota, rVec, OUT.STEPS, array(0, c(OUT.STEPS,  NG, AG, DS, TS)))[[4]])
 }
