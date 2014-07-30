7b95281d4c2aa83a78dfd737898dab027006216b
jeffeaton <jeffrey.eaton@imperial.ac.uk>
2014-07-30 22:47:54 +0100

    revised incr() function to be much faster

--- R/spectrum.R
+++ R/spectrum.R
@@ -1,4 +1,4 @@
-incr <- function(e1,e2){ return(eval.parent(substitute(e1 <- e1 + (e2))))}  # implement increment operator
+"incr<-" <- function(x, value) { x + value } # increment operator, from Hmisc
 
 NG <- 2   # number of sexes
 AG <- 17  # number of age groups
@@ -28,19 +28,19 @@ fnSpectrum <- function(iota, rVec, dt = 0.1){
     grad <- array(0, c(NG, AG, DS, TS))
 
     ## ageing
-    incr(grad[,-AG,,], -X[,-AG,,]*0.2)  # ageing
-    incr(grad[,-1,,], X[,-AG,,]*0.2)    # ageing
+    incr(grad[,-AG,,]) <- -X[,-AG,,]*0.2  # ageing
+    incr(grad[,-1,,]) <- X[,-AG,,]*0.2    # ageing
 
     ## natural mortality
-    incr(grad[1,,,], -X[1,,,]*mx.male[,year.idx])    # male mortality
-    incr(grad[2,,,], -X[2,,,]*mx.female[,year.idx])  # female mortality
+    incr(grad[1,,,]) <- -X[1,,,]*mx.male[,year.idx]    # male mortality
+    incr(grad[2,,,]) <- -X[2,,,]*mx.female[,year.idx]  # female mortality
 
     ## fertility
     births.by.age <- rowSums(X[2,fert.idx,,])*asfr[,year.idx]
     frac.hivp.births <- vert.trans*(1.0 - (rowSums(X[2, fert.idx, 1,]))/(fert.rat*rowSums(X[2, fert.idx, -1,]) + rowSums(X[2, fert.idx, 1,]))) # fraction of births HIV+ by age group
 
-    incr(grad[,1,-1,1], sum(births.by.age*frac.hivp.births) * c(srb[year.idx], 100)/(srb[year.idx]+100) * cd4.initdist[,1,]) # HIV+ children born
-    incr(grad[,1,1,1], sum(births.by.age*(1.0 - frac.hivp.births)) * c(srb[year.idx], 100)/(srb[year.idx]+100)) # HIV- children born
+    incr(grad[,1,-1,1]) <- sum(births.by.age*frac.hivp.births) * c(srb[year.idx], 100)/(srb[year.idx]+100) * cd4.initdist[,1,] # HIV+ children born
+    incr(grad[,1,1,1]) <- sum(births.by.age*(1.0 - frac.hivp.births)) * c(srb[year.idx], 100)/(srb[year.idx]+100) # HIV- children born
 
 
     if(ts >= t0){
@@ -49,15 +49,15 @@ fnSpectrum <- function(iota, rVec, dt = 0.1){
       inc.rr <- rbind(inc.agerat.male[,year.idx], inc.sexrat[year.idx]*inc.agerat.female[,year.idx])
       age.inc <- inc.rr*inc.rate.15to49/(sum(X[,age15to49.idx,1,1]*inc.rr[,age15to49.idx])/sum(X[,age15to49.idx,1,1]))
       
-      incr(grad[,,1,1], -age.inc*X[,,1,1]) # remove incident infections from susceptibles
-      incr(grad[,,-1,1], array(apply(cd4.initdist, 3, "*", age.inc * X[,,1,1]), c(NG, AG, DS-1))) # add incident infections
+      incr(grad[,,1,1]) <- -age.inc*X[,,1,1] # remove incident infections from susceptibles
+      incr(grad[,,-1,1]) <- array(apply(cd4.initdist, 3, "*", age.inc * X[,,1,1]), c(NG, AG, DS-1)) # add incident infections
       
       ## disease progression and mortality
-      incr(grad[,,-c(1,DS),1], -cd4.prog * X[,,-c(1, DS),1])    # remove cd4 stage progression (untreated)
-      incr(grad[,,-(1:2),1], cd4.prog * X[,,-c(1, DS),1])       # add cd4 stage progression (untreated)
-      incr(grad[,,-1, 2:3], -2 * X[,,-1, 2:3])                  # remove ART duration progression (HARD CODED 6 months duration)
-      incr(grad[,,-1, 3:4], 2 * X[,,-1, 2:3])                   # add ART duration progression (HARD CODED 6 months duration)
-      incr(grad[,,-1,], - cd4.art.mort * X[,,-1,])              # HIV mortality
+      incr(grad[,,-c(1,DS),1]) <- -cd4.prog * X[,,-c(1, DS),1]    # remove cd4 stage progression (untreated)
+      incr(grad[,,-(1:2),1]) <- cd4.prog * X[,,-c(1, DS),1]       # add cd4 stage progression (untreated)
+      incr(grad[,,-1, 2:3]) <- -2 * X[,,-1, 2:3]                  # remove ART duration progression (HARD CODED 6 months duration)
+      incr(grad[,,-1, 3:4]) <- 2 * X[,,-1, 2:3]                   # add ART duration progression (HARD CODED 6 months duration)
+      incr(grad[,,-1,]) <- - cd4.art.mort * X[,,-1,]              # HIV mortality
       
       ## ART initiation
       numart.15plus.ts <- sum(c(ts %% 1 + dt, 1-(ts %% 1 + dt)) * artnum.15plus[c(year.idx, max(year.idx-1, 1))])
@@ -71,13 +71,13 @@ fnSpectrum <- function(iota, rVec, dt = 0.1){
         artinit.ann.ts <- art.15plus.anninits * artinit.weight * artelig.15plus
         artinit.ann.ts[artinit.ann.ts > artelig.15plus/dt] <- artelig.15plus[artinit.ann.ts > artelig.15plus/dt]/dt  # check that don't initiate more than the number eligible
       
-        incr(grad[,age15plus.idx, artelig.idx[year.idx]:DS, 1], -artinit.ann.ts)
-        incr(grad[,age15plus.idx, artelig.idx[year.idx]:DS, 2], artinit.ann.ts)
+        incr(grad[,age15plus.idx, artelig.idx[year.idx]:DS, 1]) <- -artinit.ann.ts
+        incr(grad[,age15plus.idx, artelig.idx[year.idx]:DS, 2]) <- artinit.ann.ts
       } # if(numart.15plus.ts > 0)
     } # if(ts >= t0)
 
     ## do projection (euler integration) ##
-    incr(X, dt*grad)
+    incr(X) <- dt*grad
   } # for(ts in proj.steps)
 
   return(X.out)
